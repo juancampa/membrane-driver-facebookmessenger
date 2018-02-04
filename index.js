7dc7d27f38a6f275bb1f1ce8075b07387b448d27
Juan Campa <juancampa@gmail.com>
2018-02-04 15:45:42 -0800

    Little bit of code hygiene

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ async function sendMessage(text) {
   };
 
   const result = await client.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.ACCESS_TOKEN}`, body);
-  console.log(result.status);
-  console.log(result.body);
+  console.log('Status', result.status);
+  console.log('Body', result.body);
 }
 
 async function onMessageReceived(event) {
@@ -76,13 +76,12 @@ async function onMessageReceived(event) {
   const { message } = event;
   if (message.text) {
     console.log('MESSAGE TEXT', message.text);
-    root.messageReceived.dispatch({ text: message.text });
+    await root.messageReceived.dispatch({ text: message.text });
   } else if (message.attachments) {
     console.log('MESSAGE ATTACHMENTS', message.attachments);
   } else if (message.quick_reply) {
     console.log('MESSAGE QUICK REPLY', message.quick_reply);
   }
-
 }
 
 export let Root = {
